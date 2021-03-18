c5f358eee0b5e48d37bd7ed463f93a18e7463a44
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-03-18 13:23:09 +0900

    Initial Commit

---
+++ Sample21-ViewSize/Sample21_ViewSizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample21_ViewSizeApp.swift
+//  Sample21-ViewSize
+//
+//  Created by keiji yamaki on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct Sample21_ViewSizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
